aec419a9db713ea14420a71f82417dbe83af43f4
murali0101 <muralimv0101@gmail.com>
2022-05-02 14:38:53 +0530

    initial setup

---
+++ src/Redux/actions.js
@@ -0,0 +1,6 @@
+// action types
+export const USERM="USER"
+// Action Creators
+export function addUser() { 
+  
+}
---
+++ src/Redux/reducer.js
@@ -0,0 +1,8 @@
+const init = {usersM:[],ordersM:[],isLoggedIn:false};
+
+export const reducer = (store = init, { type, payload }) => {
+  switch (type) {
+    default:
+      return store;
+  }
+};
