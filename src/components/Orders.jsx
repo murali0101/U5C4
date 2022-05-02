cc6a59c50b2ef9e5338006c91e3d1fc163f4cafa
murali0101 <muralimv0101@gmail.com>
2022-05-02 16:26:08 +0530

    order

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux";
+
 export const Orders = () => {
   //  Get all data when admin logs in and populate it
   // store it in redux
-
+  const orderM = useSelector((store) => store.ordersM);
+  console.log("orderM:", orderM);
   return (
     <div>
       <div>
@@ -25,27 +28,31 @@ export const Orders = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="orders-row">
-              <td className="id"></td>
-              <td className="problem"></td>
-              <td className="owner"></td>
-              <td className="status"></td>
-              <td className="cost"></td>
-              <td className="change-status">
-                {/* Show select dropdown only if status is Not Accepted */}
-                <select className="changeStatus" name="changeStatus">
-                  <option value="Pending">Pending</option>
-                  <option value="In Progress">In Progress</option>
-                  <option value="Done">Done</option>
-                  <option value="Not Accepted">Not Accepted</option>
-                </select>
-              </td>
-              <td className="accept">
-                {/* Show this button only if status is Not Accepted */}
-                {/* on change make request to update it in db, and show changed status in table */}
-                <button>Accept</button>
-              </td>
-            </tr>
+            {orderM.map((e) => {
+              return (
+                <tr className="orders-row">
+                  <td className="id">{e.id}</td>
+                  <td className="problem">{e.problem}</td>
+                  <td className="owner">{e.owner_name}</td>
+                  <td className="status">{e.status}</td>
+                  <td className="cost">{e.cost}</td>
+                  <td className="change-status">
+                    {/* Show select dropdown only if status is Not Accepted */}
+                    <select className="changeStatus" name="changeStatus">
+                      <option value="Pending">Pending</option>
+                      <option value="In Progress">In Progress</option>
+                      <option value="Done">Done</option>
+                      <option value="Not Accepted">Not Accepted</option>
+                    </select>
+                  </td>
+                  <td className="accept">
+                    {/* Show this button only if status is Not Accepted */}
+                    {/* on change make request to update it in db, and show changed status in table */}
+                    <button>Accept</button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
